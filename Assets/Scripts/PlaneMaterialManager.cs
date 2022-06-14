eaaac1c8eac71ca0cd6cfda56284ee01b5442e84
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-06-15 00:52:59 +0530

    Add files via upload

---
+++ Assets/Scripts/PlaneMaterialManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneMaterialManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Material _floorMaterial;
+    [SerializeField]
+    private Button[] _buttons;
+    [SerializeField]
+    private Texture[] _textures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _buttons[0].onClick.AddListener(() => onClickButtonZero());
+        _buttons[1].onClick.AddListener(() => onClickButtonOne());
+        _buttons[2].onClick.AddListener(() => onClickButtonTwo());
+        _buttons[3].onClick.AddListener(() => onClickButtonThree());
+        _buttons[4].onClick.AddListener(() => onClickButtonFour());
+        _buttons[5].onClick.AddListener(() => onClickButtonFive());
+    }
+
+
+    void onClickButtonZero() {
+        _floorMaterial.SetTexture("_MainTex", _textures[0]);
+    }
+
+    void onClickButtonOne()
+    {
+        _floorMaterial.SetTexture("_MainTex", _textures[1]);
+    }
+
+    void onClickButtonTwo()
+    {
+        _floorMaterial.SetTexture("_MainTex", _textures[2]);
+    }
+
+    void onClickButtonThree()
+    {
+        _floorMaterial.SetTexture("_MainTex", _textures[3]);
+    }
+
+    void onClickButtonFour()
+    {
+        _floorMaterial.SetTexture("_MainTex", _textures[4]);
+    }
+
+    void onClickButtonFive()
+    {
+        _floorMaterial.SetTexture("_MainTex", _textures[5]);
+    }
+}
